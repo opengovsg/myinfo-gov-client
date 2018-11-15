0a37e0c92d84f6919dffc6ef3cf57df04056a560
Yuan Ruo <yuanruo@data.gov.sg>
2018-11-15 10:41:05 +0800

    Add tests for request parameters - Tests for request method, headers, uri and query strings - Tests for construction of Authorization header - Divide unit test between making of the API response, and parsing received data.

--- test/src/MyInfoGovClient.test.js
+++ test/src/MyInfoGovClient.test.js
@@ -61,6 +61,8 @@ describe('MyInfoGovClient', function () {
     let singpassEserviceId
     let privateKey
     let uinFin
+    let myInfoClient
+    let params
 
     beforeEach(function () {
       // Your application configuration
@@ -72,9 +74,64 @@ describe('MyInfoGovClient', function () {
 
       // request parameters
       uinFin = 'S3000024B'
+      params = {
+        uinFin,
+        requestedAttributes: ALL_ATTRIBUTES,
+      }
     })
 
-    it('should make API call and parse response correctly', function () {
+    it('should make API call with correct parameters', function () {
+      this.timeout(5000)
+
+      let requestStub = function (requestDetails, callback) {
+        expect(requestDetails.method).to.equal('GET')
+        expect(requestDetails.headers['Content-Type']).to.equal('application/json')
+        expect(requestDetails.headers['Content-Encoding']).to.equal('gzip')
+        expect(requestDetails.headers['Accept']).to.equal('application/json')
+        expect(requestDetails.uri).to.equal(`https://myinfosgstg.api.gov.sg/gov/dev/v1/person-basic/${uinFin}/`)
+        expect(requestDetails.qs.attributes).to.equal('name,marriedname,hanyupinyinname,aliasname,hanyupinyinaliasname,sex,race,dialect,nationality,dob,birthcountry,secondaryrace,residentialstatus,passportnumber,passportexpirydate,email,mobileno,regadd,housingtype,hdbtype,mailadd,billadd,marital,edulevel,marriagecertno,countryofmarriage,marriagedate,divorcedate,childrenbirthrecords,relationships,edulevel,gradyear,schoolname,occupation,employment,workpassstatus,workpassexpirydate,householdincome,vehno')
+        expect(requestDetails.qs.client_id).to.equal('STG2-MYORG-MYAPP-SP')
+        expect(requestDetails.qs.singpassEserviceId).to.equal('MYORG-MYAPP-SP')
+        expect(requestDetails.qs.txnNo).to.be.undefined
+
+        // Testing Authorization header
+        let authHeaderObj = {}
+        requestDetails.headers['Authorization']
+          .split(',')
+          .map((keyValueString) => {
+            let [key, value] = keyValueString.split('=')
+            authHeaderObj[key] = value.replace(/"/g, '')
+          })
+
+        expect(authHeaderObj['Apex_l2_Eg realm']).to.equal('MYAPP')
+        expect(authHeaderObj['apex_l2_eg_app_id']).to.equal('STG2-MYORG-MYAPP-SP')
+        expect(authHeaderObj['apex_l2_eg_signature_method']).to.equal('SHA256withRSA')
+        expect(authHeaderObj['apex_l2_eg_version']).to.equal('1.0')
+        expect(authHeaderObj['apex_l2_eg_signature']).to.exist
+      }
+
+      let MyInfoGovClient = proxyquire(
+        '../../src/MyInfoGovClient.class.js',
+        { request: requestStub }
+      )
+
+      myInfoClient = new MyInfoGovClient({
+        realm,
+        appId,
+        clientId,
+        singpassEserviceId,
+        privateKey,
+        mode: 'dev',
+      })
+
+      myInfoClient.getPersonBasic(params)
+        .catch(error => {
+          console.error(error)
+          throw new Error(error)
+        })
+    })
+
+    it('should parse the response correctly', function (done) {
       let requestStub = function (requestDetails, callback) {
         let response = { statusCode: 200 }
         callback(undefined, response, SAMPLE_RESPONSE)
@@ -85,7 +142,7 @@ describe('MyInfoGovClient', function () {
         { request: requestStub }
       )
 
-      let myInfoClient = new MyInfoGovClient({
+      myInfoClient = new MyInfoGovClient({
         realm,
         appId,
         clientId,
@@ -105,6 +162,7 @@ describe('MyInfoGovClient', function () {
           { uinFin }
         )
         expect(personObject).to.deep.equal(expectedPersonObject)
+        done()
       })
     })
   })
