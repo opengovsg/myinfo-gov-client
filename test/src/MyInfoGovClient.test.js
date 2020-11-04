a394c631d116d8ed22d27f52698e1692705173c5
Antariksh <antarikshmahajan@gmail.com>
2020-11-04 09:25:55 +0800

    test: update tests to use axios

--- test/src/MyInfoGovClient.test.js
+++ test/src/MyInfoGovClient.test.js
@@ -11,6 +11,11 @@ const SAMPLE_RESPONSE = fs.readFileSync(
   path.resolve(__dirname, '../resources/response.txt'),
 )
 
+const MOCK_RESPONSE = {
+  statusCode: 200,
+  data: JSON.stringify({ mockKey: 'mockValue' }),
+}
+
 const ALL_ATTRIBUTES = [
   'name',
   'marriedname',
@@ -80,40 +85,41 @@ describe('MyInfoGovClient', function () {
       }
     })
 
-    it('should make API call with correct parameters', function () {
+    it('should make API call with correct parameters', async function () {
       this.timeout(5000)
 
-      let requestStub = function (requestDetails, callback) {
-        expect(requestDetails.method).to.equal('GET')
-        expect(requestDetails.headers['Content-Type']).to.equal('application/json')
-        expect(requestDetails.headers['Content-Encoding']).to.equal('gzip')
-        expect(requestDetails.headers['Accept']).to.equal('application/json')
-        expect(requestDetails.uri).to.equal(`https://myinfosgstg.api.gov.sg/gov/dev/v1/person-basic/${uinFin}/`)
-        expect(requestDetails.qs.attributes).to.equal('name,marriedname,hanyupinyinname,aliasname,hanyupinyinaliasname,sex,race,dialect,nationality,dob,birthcountry,secondaryrace,residentialstatus,passportnumber,passportexpirydate,email,mobileno,regadd,housingtype,hdbtype,mailadd,billadd,marital,edulevel,marriagecertno,countryofmarriage,marriagedate,divorcedate,childrenbirthrecords,relationships,edulevel,gradyear,schoolname,occupation,employment,workpassstatus,workpassexpirydate,householdincome,vehno')
-        expect(requestDetails.qs.client_id).to.equal('STG2-MYORG-MYAPP-SP')
-        expect(requestDetails.qs.singpassEserviceId).to.equal('MYORG-MYAPP-SP')
-        expect(requestDetails.qs.txnNo).to.be.undefined
+      const axiosStub = {
+        get: function (url, options) {
+          expect(options.headers['Content-Type']).to.equal('application/json')
+          expect(options.headers['Accept']).to.equal('application/json')
+          expect(url).to.equal(`https://myinfosgstg.api.gov.sg/gov/dev/v1/person-basic/${uinFin}/`)
+          expect(options.params.attributes).to.equal('name,marriedname,hanyupinyinname,aliasname,hanyupinyinaliasname,sex,race,dialect,nationality,dob,birthcountry,secondaryrace,residentialstatus,passportnumber,passportexpirydate,email,mobileno,regadd,housingtype,hdbtype,mailadd,billadd,marital,edulevel,marriagecertno,countryofmarriage,marriagedate,divorcedate,childrenbirthrecords,relationships,edulevel,gradyear,schoolname,occupation,employment,workpassstatus,workpassexpirydate,householdincome,vehno')
+          expect(options.params.client_id).to.equal('STG2-MYORG-MYAPP-SP')
+          expect(options.params.singpassEserviceId).to.equal('MYORG-MYAPP-SP')
+          expect(options.params.txnNo).to.be.undefined
 
-        // Testing Authorization header
-        let authHeaderObj = {}
-        requestDetails.headers['Authorization']
-          .split(',')
-          .map((keyValueString) => {
-            let [key, value] = keyValueString.split('=')
-            authHeaderObj[key] = value.replace(/"/g, '')
-          })
+          // Testing Authorization header
+          let authHeaderObj = {}
+          options.headers['Authorization']
+            .split(',')
+            .map((keyValueString) => {
+              let [key, value] = keyValueString.split('=')
+              authHeaderObj[key] = value.replace(/"/g, '')
+            })
 
-        expect(authHeaderObj['Apex_l2_Eg realm']).to.equal('MYAPP')
-        expect(authHeaderObj['apex_l2_eg_app_id']).to.equal('STG2-MYORG-MYAPP-SP')
-        expect(authHeaderObj['apex_l2_eg_signature_method']).to.equal('SHA256withRSA')
-        expect(authHeaderObj['apex_l2_eg_version']).to.equal('1.0')
-        expect(authHeaderObj['apex_l2_eg_signature']).to.exist
-        expect(authHeaderObj['apex_l2_eg_timestamp']).to.exist
+          expect(authHeaderObj['Apex_l2_Eg realm']).to.equal('MYAPP')
+          expect(authHeaderObj['apex_l2_eg_app_id']).to.equal('STG2-MYORG-MYAPP-SP')
+          expect(authHeaderObj['apex_l2_eg_signature_method']).to.equal('SHA256withRSA')
+          expect(authHeaderObj['apex_l2_eg_version']).to.equal('1.0')
+          expect(authHeaderObj['apex_l2_eg_signature']).to.exist
+          expect(authHeaderObj['apex_l2_eg_timestamp']).to.exist
+          return Promise.resolve(MOCK_RESPONSE)
+        },
       }
 
       const { MyInfoGovClient } = proxyquire(
         '../../build/MyInfoGovClient.class.js',
-        { request: requestStub },
+        { axios: axiosStub },
       )
 
       myInfoClient = new MyInfoGovClient({
@@ -125,22 +131,19 @@ describe('MyInfoGovClient', function () {
         mode: 'dev',
       })
 
-      myInfoClient.getPersonBasic(params)
-        .catch(error => {
-          console.error(error)
-          throw new Error(error)
-        })
+      await myInfoClient.getPersonBasic(params)
     })
 
     it('should parse the response correctly', function (done) {
-      let requestStub = function (requestDetails, callback) {
-        let response = { statusCode: 200 }
-        callback(undefined, response, SAMPLE_RESPONSE)
+      const axiosStub = {
+        get: function () {
+          return Promise.resolve({ data: SAMPLE_RESPONSE.toString() })
+        },
       }
 
       const { MyInfoGovClient } = proxyquire(
         '../../build/MyInfoGovClient.class.js',
-        { request: requestStub },
+        { axios: axiosStub },
       )
 
       myInfoClient = new MyInfoGovClient({
