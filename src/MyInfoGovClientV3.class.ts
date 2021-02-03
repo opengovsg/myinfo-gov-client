849c688da7eff1c0e4bbc6e00ed885a2468eedfc
Antariksh <antarikshmahajan@gmail.com>
2021-02-03 18:24:05 +0800

    feat: implement createRedirectURL

--- src/MyInfoGovClientV3.class.ts
+++ src/MyInfoGovClientV3.class.ts
@@ -1,1 +1,78 @@
-export class MyInfoGovClient {}
+import qs from 'qs'
+
+export enum MyInfoMode {
+  Dev = 'dev',
+  Staging = 'stg',
+  Production = 'prod',
+}
+
+const BASE_URL: { [M in MyInfoMode]: string } = {
+  [MyInfoMode.Dev]: 'http://localhost:5156/myinfo/v3',
+  [MyInfoMode.Staging]: 'https://myinfosgstg.api.gov.sg/gov/test/v3',
+  [MyInfoMode.Production]: 'https://myinfosg.api.gov.sg/gov/v3',
+}
+
+export interface IMyInfoConfig {
+  clientId: string
+  clientSecret: string
+  singpassEserviceId: string
+  mode?: MyInfoMode
+}
+
+export interface IPersonRequest {
+  purpose: string
+  requestedAttributes: string[]
+  relayState: string
+  targetUrl: string
+  singpassEserviceId?: string
+}
+
+enum Endpoint {
+  Authorise = '/authorise',
+  Token = '/token',
+  Person = '/person',
+}
+
+export class MyInfoGovClient {
+  clientId: string
+  clientSecret: string
+  mode: MyInfoMode
+  singpassEserviceId: string
+  baseUrl: string
+
+  constructor(config: IMyInfoConfig) {
+    const { clientId, clientSecret, mode, singpassEserviceId } = config
+
+    if (!clientId || !clientSecret || !singpassEserviceId) {
+      throw new Error(
+        'Missing required parameter(s) in constructor:' +
+          ' clientId, clientSecret',
+      )
+    }
+
+    this.clientId = clientId
+    this.clientSecret = clientSecret
+    this.mode = mode || MyInfoMode.Production
+    this.singpassEserviceId = singpassEserviceId
+    this.baseUrl = BASE_URL[this.mode] || BASE_URL.prod
+  }
+
+  createRedirectURL({
+    purpose,
+    targetUrl,
+    relayState,
+    requestedAttributes,
+    singpassEserviceId,
+  }: IPersonRequest): string {
+    const eSrvcIdDefined = singpassEserviceId ?? this.singpassEserviceId
+    const queryParams = {
+      purpose,
+      attributes: requestedAttributes.join(),
+      state: relayState,
+      redirect_uri: targetUrl,
+      client_id: this.clientId,
+      sp_esvcId: eSrvcIdDefined,
+    }
+    return `${this.baseUrl}${Endpoint.Authorise}?${qs.stringify(queryParams)}`
+  }
+}
--- test/src/MyInfoGovClient.test.ts
+++ test/src/MyInfoGovClient.test.ts
@@ -1,10 +1,80 @@
-import { MyInfoGovClient } from '../../src/MyInfoGovClientV3.class'
+import axios from 'axios'
+import { MyInfoGovClient, MyInfoMode } from '../../src/MyInfoGovClientV3.class'
+
+export const MOCK_TARGET_URL = 'https://myinfo.gov.client/target'
+export const MOCK_CLIENT_ID = 'mockClientId'
+export const MOCK_CLIENT_SECRET = 'mockClientSecret'
+export const MOCK_ESRVC_ID = 'mockEsrvcId'
+export const MOCK_PURPOSE = 'purpose'
+export const MOCK_RELAY_STATE = 'relayState1,relayState2'
+export const MOCK_REQUESTED_ATTRIBUTES = [
+  'name',
+  'sex',
+  'mobileno',
+  'occupation',
+  'marital',
+]
 
 describe('MyInfoGovClient', () => {
+  const client = new MyInfoGovClient({
+    clientId: MOCK_CLIENT_ID,
+    clientSecret: MOCK_CLIENT_SECRET,
+    mode: MyInfoMode.Dev,
+    singpassEserviceId: MOCK_ESRVC_ID,
+  })
   describe('constructor', () => {
     it('should instantiate without errors', () => {
-      const client = new MyInfoGovClient()
       expect(client).toBeTruthy()
+    })
+  })
+
+  describe('createRedirectURL', () => {
+    it('should create the correct redirect URL when e-service ID is not specified', () => {
+      const redirectUrl = client.createRedirectURL({
+        purpose: MOCK_PURPOSE,
+        relayState: MOCK_RELAY_STATE,
+        requestedAttributes: MOCK_REQUESTED_ATTRIBUTES,
+        targetUrl: MOCK_TARGET_URL,
+      })
+      const parsedUrl = new URL(redirectUrl)
+      const queryParams = parsedUrl.searchParams
+
+      expect(parsedUrl.origin + parsedUrl.pathname).toBe(
+        client.baseUrl + '/authorise',
+      )
+      expect(queryParams.getAll('purpose')).toEqual([MOCK_PURPOSE])
+      expect(queryParams.getAll('attributes')).toEqual([
+        MOCK_REQUESTED_ATTRIBUTES.join(','),
+      ])
+      expect(queryParams.getAll('state')).toEqual([MOCK_RELAY_STATE])
+      expect(queryParams.getAll('redirect_uri')).toEqual([MOCK_TARGET_URL])
+      expect(queryParams.getAll('client_id')).toEqual([MOCK_CLIENT_ID])
+      expect(queryParams.getAll('sp_esvcId')).toEqual([MOCK_ESRVC_ID])
+    })
+
+    it('should create the correct redirect URL when e-service ID is specified', () => {
+      const mockEsrvcId = 'someOtherEsrvcId'
+      const redirectUrl = client.createRedirectURL({
+        purpose: MOCK_PURPOSE,
+        relayState: MOCK_RELAY_STATE,
+        requestedAttributes: MOCK_REQUESTED_ATTRIBUTES,
+        targetUrl: MOCK_TARGET_URL,
+        singpassEserviceId: mockEsrvcId,
+      })
+      const parsedUrl = new URL(redirectUrl)
+      const queryParams = parsedUrl.searchParams
+
+      expect(parsedUrl.origin + parsedUrl.pathname).toBe(
+        client.baseUrl + '/authorise',
+      )
+      expect(queryParams.getAll('purpose')).toEqual([MOCK_PURPOSE])
+      expect(queryParams.getAll('attributes')).toEqual([
+        MOCK_REQUESTED_ATTRIBUTES.join(','),
+      ])
+      expect(queryParams.getAll('state')).toEqual([MOCK_RELAY_STATE])
+      expect(queryParams.getAll('redirect_uri')).toEqual([MOCK_TARGET_URL])
+      expect(queryParams.getAll('client_id')).toEqual([MOCK_CLIENT_ID])
+      expect(queryParams.getAll('sp_esvcId')).toEqual([mockEsrvcId])
     })
   })
 })
