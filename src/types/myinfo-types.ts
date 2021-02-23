32b4fc9bac9a33c419a96660923751e7f3d623b3
Yuanruo Liang <yuanruo@open.gov.sg>
2021-02-23 16:53:54 +0800

    refactor(vehicles): extract into own file

--- src/types/myinfo-types.ts
+++ src/types/myinfo-types.ts
@@ -4,51 +4,10 @@ import { MyInfoChildrenBirthRecord, ChildCustomFields } from "./childrenbirthrec
 import { MyInfoHdbOwnership } from "./hdbownership"
 import { MyInfoHouseholdIncome } from "./householdincome"
 import { MyInfoPhoneNumber } from "./mobileno"
+import { MyInfoVehicle, MyInfoVehicleCustomFields } from "./vehicles"
 import { MyInfoOccupation } from "./occupation"
 import { MyInfoOwnerPrivate } from "./ownerprivate"
 import { MyInfoSponsoredChildrenRecord, SponsoredChildCustomFields } from "./sponsoredchildrenrecords"
-
-type MyInfoVehicleCustomFields = {
-  vehicleno: StringValue
-  type: StringValue
-  iulabelno: StringValue
-  make: StringValue
-  model: StringValue
-  chassisno: StringValue
-  engineno: StringValue
-  motorno: StringValue
-  yearofmanufacture: StringValue
-  firstregistrationdate: StringValue
-  originalregistrationdate: StringValue
-  coecategory: StringValue
-  coeexpirydate: StringValue
-  roadtaxexpirydate: StringValue
-  quotapremium: NumberValue
-  openmarketvalue: NumberValue
-  co2emission: NumberValue
-  status: CodeAndDesc
-  primarycolour: StringValue
-  secondarycolour: StringValue
-  attachment1: StringValue
-  attachment2: StringValue
-  attachment3: StringValue
-  scheme: StringValue
-  thcemission: NumberValue
-  coemission: NumberValue
-  noxemission: NumberValue
-  pmemission: NumberValue
-  enginecapacity: NumberValue
-  powerrate: NumberValue
-  effectiveownership: StringValue
-  propellant: StringValue
-  maximumunladenweight: NumberValue
-  maximumladenweight: NumberValue
-  minimumparfbenefit: NumberValue
-  nooftransfers: NumberValue
-  vpc: StringValue
-}
-
-export type MyInfoVehicle = MyInfoField<MyInfoVehicleCustomFields>
 
 type StartEndDate = {
   startdate: StringValue
